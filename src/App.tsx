6e72288398d14b9cef0fe79dcda2cd3bf476bee9
Rena <choco.rena0609@gmail.com>
2020-06-11 06:40:10 +0900

    動かない

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const todos = [
+  {
+    id: 1,
+    text: "Do laundry",
+    done: false,
+    place: "home",
+  },
+  {
+    id: 2,
+    text: "Email boss",
+    done: false,
+    place: "work",
+  },
+  {
+    id: 3,
+    text: "Go to gym",
+    done: false,
+    place: { custom: "Gym" },
+  },
+  {
+    id: 4,
+    text: "Buy milk",
+    done: false,
+    place: { custom: "Supermarket" },
+  },
+  { id: 5, text: "Read a book", done: false },
+];
+
+type Place = "home" | "work" | { custom: string };
+
+type Todo = Readonly<{
+  id: number;
+  text: string;
+  done: boolean;
+  place?: Place;
+}>;
+
+type CompletedTodo = Todo & {
+  readonly done: true;
+};
+
+function toggleTodo(todo: Todo): Todo {
+  console.log(todo);
+  return {
+    id: todo.id,
+    text: todo.text,
+    done: !todo.done,
+    place: todo.place,
+  };
+}
+
+function completeAll(todos: readonly Todo[]): CompletedTodo[] {
+  console.log("completed");
+  return todos.map((todo) => ({
+    ...todo,
+    done: true,
+  }));
+}
+
+function placeToString(place: Place): string {
+  if (place === "home") {
+    return "🏠Home";
+  } else if (place === "work") {
+    return " 👜Work";
+  } else {
+    return "❗️" + place.custom;
+  }
+}
+placeToString({ custom: "Gym" });
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todos.map((todo, index) => (
+        <div key={index}>
+          <span role="button" onClick={() => toggleTodo}>
+            check
+          </span>
+          <div>{todo.text}</div>
+          {todo.place && <div>{}</div>}
+        </div>
+      ))}
+      <span role="button" onClick={() => completeAll}>
+        check All
+      </span>
     </div>
   );
 }
